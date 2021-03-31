48e8925fa189bab300c2aaf821eedd0418930511
Sergei Gavrish <sd.gavrish@gmail.com>
2021-04-01 00:11:34 +0300

    [SPRINT-2] IUserRepository interface was added

---
+++ sprint_2/src/user/repositories/iuser-repository.ts
@@ -0,0 +1,9 @@
+import { IUser } from '../interfaces/iuser';
+
+export interface IUserRepository {
+    readById(id: string): Promise<IUser | null>;
+    readByLogin(login: string, limit: number): Promise<IUser[]>;
+    create(user: IUser): Promise<IUser>;
+    update(user: IUser): Promise<IUser>;
+    delete(id: string): Promise<boolean>;
+}
