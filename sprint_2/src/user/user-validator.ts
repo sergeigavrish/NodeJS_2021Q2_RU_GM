293031b88b1fabda59ebfeac10cd0e396a8663e7
Sergei Gavrish <sd.gavrish@gmail.com>
2021-04-06 03:17:56 +0300

    [SPRINT-2] update user validators

--- sprint_2/src/user/user-validator.ts
+++ sprint_2/src/user/user-validator.ts
@@ -1,21 +1,21 @@
 import { createValidator } from 'express-joi-validation';
 import { number, object, string } from 'joi';
-import { IUserDto } from './interfaces/iuser-dto';
+import { ICreateUserDto, IUpdateUserDto } from './interfaces/iuser-dto';
 import { IUserId } from './interfaces/iuser-id';
 
-const userValidator = createValidator();
+const userValidator = createValidator({ passError: true });
 
-const userDtoScheme = {
+const createUserDto = object<ICreateUserDto>({
     login: string().min(1).required(),
     password: string().regex(/^(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$/i).required(),
     age: number().min(4).max(130).required()
-};
+});
 
-const createUserDto = object<IUserDto>(userDtoScheme);
-
-const updateUserDto = object<Required<IUserDto>>({
+const updateUserDto = object<IUpdateUserDto>({
     id: string().guid().required(),
-    ...userDtoScheme
+    login: string().min(1),
+    password: string().regex(/^(?:[0-9]+[a-z]|[a-z]+[0-9])[a-z0-9]*$/i),
+    age: number().min(4).max(130)
 });
 
 const userIdParam = object<IUserId>({
